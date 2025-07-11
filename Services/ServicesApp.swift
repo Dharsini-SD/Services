b850fd9c4c11f6026a4361d3039cbb7fd0e08004
Dharsini <dharsinis2000@gmail.com>
2025-07-11 16:14:55 +0530

    Initial Commit

---
+++ Services/ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesApp.swift
+//  Services
+//
+//  Created by Dharsini S on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
